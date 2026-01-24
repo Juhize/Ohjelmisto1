3011b0883db67e2fab0d9274b52e3081e2cdd722
Juhize <168896778+Juhize@users.noreply.github.com>
2026-01-24 09:07:46 +0200

    kotona

--- Tehtäviä4/4_5.py
+++ Tehtäviä4/4_5.py
@@ -4,9 +4,13 @@
 
 käyttäjä = str
 salasana = str
+kerrat = 0
 while käyttäjä != str("python") or salasana != str("rules"):
+    if kerrat >=5:
+        break
     käyttäjä = str(input("Käyttäjätunnus: "))
     salasana = str(input("Salasana: "))
     if käyttäjä != str("python") or salasana != str("rules"):
         print("Pääsy evätty.")
-print("Tervetuloa!")
+    else: print("Tervetuloa!")
+    kerrat = kerrat + 1
