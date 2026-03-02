290eac4939051d8593f9803637cfc941dedcda41
la-juhani <juhize21@gmail.com>
2026-03-02 12:34:31 +0200

    Koululla

--- Tehtäviä7/7_1.py
+++ Tehtäviä7/7_1.py
@@ -3,3 +3,8 @@
 #Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina 
 #monikkotietorakenteeseen. Määritellään kukin vuodenaika kolmen kuukauden 
 #mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
+
+kuukausi_nro = int(input("Kuukausi numero: "))
+kuukaudet =["talvi","talvi","kevät","kevät","kevät","kesä", "kesä","kesä","syksy","syksy","syksy","talvi"]
+vuodenaika = kuukaudet[kuukausi_nro-1]
+print (f"Vuodenaika on {vuodenaika}.")
