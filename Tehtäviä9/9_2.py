136def80f8925042410a7099cf14553c6b7116c5
la-juhani <juhize21@gmail.com>
2026-03-17 15:07:26 +0200

    2 valmis

--- Tehtäviä9/9_2.py
+++ Tehtäviä9/9_2.py
@@ -8,3 +8,34 @@
 #Tulosta tämän jälkeen auton nopeus. Tee sitten hätäjarrutus määräämällä
 #nopeuden muutos -200 km/h ja tulosta uusi nopeus. Kuljettua matkaa 
 #ei tarvitse vielä päivittää.
+
+class Car:
+    def __init__(self, plate, max_speed, current_speed = 0, distance = 0):
+        self.plate = plate
+        self.max_speed = max_speed
+        self.current_speed = current_speed
+        self.distance = distance
+
+    def accelerate(self, v0, v1):
+        self.current_speed = self.current_speed + v1
+        if self.current_speed < 0:
+            self.current_speed=0
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+
+        if v1 < 0:
+            print("Auto hidastaa")
+        elif v1 > 0:
+            print("Auto kiihdyttää")
+        else:
+            print("Auton kiihtyvyys on 0")
+        return self.current_speed
+
+car1 = Car("ABC-123", 142)
+
+car1.accelerate(car1.current_speed, 30)
+print(f"{car1.current_speed}")
+car1.accelerate(car1.current_speed, 70)
+print(f"{car1.current_speed}")
+car1.accelerate(car1.current_speed, -200)
+print(f"{car1.current_speed}")
