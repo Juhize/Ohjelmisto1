63981c52374400729aee3093c4476ce5e5914f43
Juhize <168896778+Juhize@users.noreply.github.com>
2026-03-21 13:48:08 +0200

    Made improved version of 10_2.

--- Tehtäviä10/10_2(dic).py
+++ Tehtäviä10/10_2(dic).py
@@ -1,14 +1,8 @@
-# Jatka edellisen tehtävän ohjelmaa siten, että teet Talo-luokan. 
-# Talon alustajaparametreina annetaan alimman ja ylimmän kerroksen numero 
-# sekä hissien lukumäärä. Talon luonnin yhteydessä talo luo tarvittavan määrän
-# hissejä. Hissien lista tallennetaan talon ominaisuutena. 
-# Kirjoita taloon metodi aja_hissiä, joka saa parametreinaan hissin numeron
-# ja kohdekerroksen. Kirjoita pääohjelmaan lauseet talon luomiseksi ja 
-# talon hisseillä ajelemiseksi.
+# Here is the previous assignment using dictionary in elevators.
 
 class Building:
     def __init__(self, lowest_floor, top_floor, num_elevators):
-        self.elevators = []
+        self.elevators = {}
         self.top_floor = top_floor
         self.lowest_floor = lowest_floor
         self.num_elevators = num_elevators
@@ -16,22 +10,24 @@ class Building:
             self.build_elevators(lowest_floor, top_floor, num_elevators)
 
     def build_elevators(self, lowest_floor, top_floor, num_elevators):
-        self.elevators.append(Elevator(f"Elevator{Elevator.noe +1}", self.lowest_floor, self.top_floor))
+        name = f"Elevator{Elevator.noe +1}"
+        self.elevators[name] = Elevator(name, self.lowest_floor, self.top_floor)
 
-    def use_elevator(name, floor):
-        Elevator.ride(name, floor)
+    def use_elevator(self, name, floor):
+        elevator = self.elevators[name]
+        Elevator.ride(elevator, floor)
 
 class Elevator:
     noe = 0 #number of elevators
     def __init__(self, name, ground_floor, roof_floor):
-        self.name = name
         self.ground = ground_floor
         self.roof = roof_floor
         self.floor = ground_floor
+        self.name = name
         Elevator.noe += 1
 
     def __str__(self):
-        return f"{self.name} Is currently in floor: {self.floor}"
+        return f"{self.name} is currently in floor: {self.floor}"
 
     def ride_up(self):
         self.floor = self.floor +1
@@ -56,7 +52,7 @@ class Elevator:
             print(self.floor)
             
 building1 = Building(-2, 10, 2)
-Building.use_elevator(building1.elevators[0], -5)
-Building.use_elevator(building1.elevators[1], 12)
+building1.use_elevator("Elevator1", -5)
+building1.use_elevator("Elevator2", 12)
 for elevator in building1.elevators:
-    print(f"{elevator}")
+    print(f"{building1.elevators[elevator]}")
