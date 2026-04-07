6969e5bca926c6b7fe3a8693aa482a5152b0f987
la-juhani <juhize21@gmail.com>
2026-04-07 12:59:57 +0300

    .

--- JavaTehtävä/Mod3/module3-starters-main/t2/2.js
+++ JavaTehtävä/Mod3/module3-starters-main/t2/2.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const targetElem = document.querySelector('#target');
+
 const felem = document.createElement('li');
 const selem = document.createElement('li');
 const telem = document.createElement('li');
+selem.classList.add('my-item');
 felem.innerHTML = 'First item';
 selem.innerHTML = 'Second item';
 telem.innerHTML = 'Third item';
