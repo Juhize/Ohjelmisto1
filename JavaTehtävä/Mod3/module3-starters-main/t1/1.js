9b72d61ef212235c8e84dc0ffe5d0425290e2cc2
la-juhani <juhize21@gmail.com>
2026-04-07 11:21:27 +0300

    .

--- JavaTehtävä/Mod3/module3-starters-main/t1/1.js
+++ JavaTehtävä/Mod3/module3-starters-main/t1/1.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const targetElem = document.querySelector('#target');
+
+targetElem.classList.add('my-list')
+
 targetElem.innerHTML =`
 <li>'First item'</li>
 <li>'Second item'</li>
---
+++ JavaTehtävä/Mod3/module3-starters-main/t2/2.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const target = document.createElement('p');
+target.innerHTML = greeting;
+document.body.appendChild(target);
