2ec43f2551a8f9f8431f4a70a7e29122e3f6216c
la-juhani <juhize21@gmail.com>
2026-04-07 13:53:34 +0300

    .

--- JavaTehtävä/Mod3/module3-starters-main/t3/3.js
+++ JavaTehtävä/Mod3/module3-starters-main/t3/3.js
@@ -1,2 +1,7 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+const targetElem = document.querySelector('#target');
+
+for (let nimi of names) {
+    targetElem.innerHTML += `<li>${nimi}</li>`;
+}
