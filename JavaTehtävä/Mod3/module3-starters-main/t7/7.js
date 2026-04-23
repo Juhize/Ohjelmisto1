ef216288b38b9413d6b6c914607f54daf9ee89fc
Juhize <168896778+Juhize@users.noreply.github.com>
2026-04-23 20:31:37 +0300

    .

--- JavaTehtävä/Mod3/module3-starters-main/t7/7.js
+++ JavaTehtävä/Mod3/module3-starters-main/t7/7.js
@@ -3,7 +3,10 @@
 const targetElem = document.querySelector('#target');
 const targetTxt = document.querySelector('#trigger');
 
-function vaihto(){
-    targetElem.src = "img/picB.jpg";
-}
-targetTxt.addEventListener('mouseover', vaihto);
+targetTxt.addEventListener('mouseover', function(over){
+targetElem.src = "img/picB.jpg";
+targetTxt.removeEventListener('mouseover');
+});
+targetTxt.addEventListener('mouseleave', function(off){
+targetElem.src = "img/picA.jpg";
+});
