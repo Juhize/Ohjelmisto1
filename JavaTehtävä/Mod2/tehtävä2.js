6765889b7e1783e6c8cb4311eb7dd59ac4fa383a
Juhize <168896778+Juhize@users.noreply.github.com>
2026-04-24 12:00:37 +0300

    .

--- JavaTehtävä/Mod2/tehtävä2.js
+++ JavaTehtävä/Mod2/tehtävä2.js
@@ -1,12 +1,18 @@
-// Write a program that asks the user for the number of participants. After this, the program asks for the names of all participants. Finally, the program prints the names of the participants on the web page in an ordered list (<ol>) in alphabetical order. (2p)
+// Write a program that asks the user for the number of participants. After this, the program asks for the names of all participants. 
+// Finally, the program prints the names of the participants on the web page in an ordered list (<ol>) in alphabetical order. (2p)
 'use strict'
-
+const userlist = document.querySelector('ol');
 const participants = [];
 let users = prompt("Give me a number of participants: ");
 
-while (users > 0){
-    let part = prompt(`Tell user ${users} name: `);
+while (Number(users) > 0){
+    let part = prompt(`Tell user number ${users} name: `);
     participants.push(part);
     users--;
 }
-console.log(participants)
+participants.sort();
+participants.forEach(function(item){
+    const user = document.createElement('li');
+    user.innerText = item;
+    userlist.appendChild(user);
+});
